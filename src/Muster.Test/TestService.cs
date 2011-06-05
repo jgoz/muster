b79b2541d9a1cd41e868d1aa69d5be9e741c2e5d
Johnny Gozde <johnny@jgoz.net>
2011-06-05 02:07:45 -0600

    Add test project.

---
+++ src/Muster.Test/TestService.cs
@@ -0,0 +1,36 @@
+﻿namespace Muster.Test
+{
+	using System;
+
+	public class TestService : IWindowsService
+	{
+		public void OnStart(string[] args)
+		{
+			Console.WriteLine("We are starting.");
+		}
+
+		public void OnStop()
+		{
+			Console.WriteLine("We are stopping.");
+		}
+
+		public void OnPause()
+		{
+			Console.WriteLine("We are pausing.");
+		}
+
+		public void OnContinue()
+		{
+			Console.WriteLine("We are resuming.");
+		}
+
+		public void OnShutdown()
+		{
+			Console.WriteLine("We are shutting down.");
+		}
+
+		public void Dispose()
+		{
+		}
+	}
+}
