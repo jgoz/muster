d2fd1445d7456d7f014e80b52b5c459aa724506f
Johnny Gozde <johnny@jgoz.net>
2011-06-21 21:38:15 -0600

    Add nuget packaging support.

--- src/VersionAssemblyInfo.cs
+++ src/VersionAssemblyInfo.cs
@@ -1,19 +1,5 @@
-using System.Reflection;
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// This project uses Semantic Versioning (http://semver.org/) to structure build
-// numbers, but modified for .NET projects.
-//
-// Following the X.Y.Z SemVer spec:
-//      Major Version = X
-//      Minor Version = Y
-//      Build Number = Target Platform (0 = AnyCPU|Windows)
-//      Revision = Z
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+using System.Reflection; 
+ 
+[assembly: AssemblyVersion("0.1.0.0")] 
+[assembly: AssemblyFileVersion("0.1.0.0")] 
+//// [assembly: AssemblyInformationalVersion("0.1.0.0 Alpha")] 
