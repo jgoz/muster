b9280f3810d9cc8fba21ad6bebacd5d63700f84a
Johnny Gozde <johnny@jgoz.net>
2011-06-21 19:16:37 -0600

    Add Global and Version assembly info files.

---
+++ src/GlobalAssemblyInfo.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCompany("Johnny Gozde")]
+[assembly: AssemblyProduct("Muster")]
+[assembly: AssemblyCopyright("Copyright © Johnny Gozde 2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(false)]
+[assembly: NeutralResourcesLanguage("en-US")]
--- src/Muster/Properties/AssemblyInfo.cs
+++ src/Muster/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -7,30 +6,6 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyTitle("Muster")]
 [assembly: AssemblyDescription("Logical Windows service abstraction, debugging, and deployment.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Johnny Gozde")]
-[assembly: AssemblyProduct("Muster")]
-[assembly: AssemblyCopyright("Copyright © Johnny Gozde 2011")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("fa5f01c8-44e7-416a-ab49-781f51781a20")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
