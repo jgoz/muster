8455d106665e7c3bc2612994ed97719ec3d34296
Johnny Gozde <johnny@jgoz.net>
2011-07-14 21:27:09 -0600

    Use ILMerge to generate muster.exe during the build process.

--- build.cmd
+++ build.cmd
@@ -21,24 +21,39 @@ if exist %publish% ( rmdir /s /q %publish% )
 mkdir output
 mkdir "output\bin"
 mkdir "output\lib"
+mkdir "output\merge"
+mkdir "output\test"
 
 echo === COMPILING ===
 echo Compiling / Target: %FRAMEWORK_VERSION% / Config: %TARGET_CONFIG%
 msbuild /nologo /verbosity:quiet src\Muster.sln /p:Configuration=%TARGET_CONFIG% /t:Clean
-msbuild /nologo /verbosity:quiet src\Muster.sln /p:Configuration=%TARGET_CONFIG% /p:TargetFrameworkVersion=%FRAMEWORK_VERSION%
+msbuild /nologo /verbosity:quiet src\Muster.sln /p:Configuration=%TARGET_CONFIG% /p:TargetFrameworkVersion=%FRAMEWORK_VERSION% /p:SignAssembly=true /p:AssemblyOriginatorKeyFile=..\Muster.snk
 
 xcopy /Q /Y src\Muster\bin\%TARGET_CONFIG%\*.* output\lib
 
 echo .xml >> exclude.txt
-xcopy /Q /Y /EXCLUDE:exclude.txt src\Muster\bin\%TARGET_CONFIG%\*.* output\bin
+xcopy /Q /Y /EXCLUDE:exclude.txt src\Muster\bin\%TARGET_CONFIG%\*.* output\merge
+
+xcopy /Q /Y /EXCLUDE:exclude.txt src\Muster.Test\bin\%TARGET_CONFIG%\*.* output\test
 
 echo .vshost.exe >> exclude.txt
 echo .manifest >> exclude.txt
 echo .config >> exclude.txt
-xcopy /Q /Y /EXCLUDE:exclude.txt src\Muster.Runner\bin\%TARGET_CONFIG%\*.* output\bin
+xcopy /Q /Y /EXCLUDE:exclude.txt src\Muster.Runner\bin\%TARGET_CONFIG%\*.* output\merge
 
 del exclude.txt
 
+if %TARGET_CONFIG%==Debug goto finalize
+
+echo.
+echo === MERGING ===
+set ILMERGE_FRAMEWORK=
+if %FRAMEWORK_VERSION%==v4.0 ( set ILMERGE_FRAMEWORK=/v4 )
+
+ilmerge %ILMERGE_FRAMEWORK% /keyfile:src\Muster.snk /out:output\bin\muster.exe output\merge\Muster.Runner.exe output\merge\Muster.dll
+rmdir /s /q output\merge
+
+:finalize
 echo.
 echo === FINALIZING ===
 move output %publish%
