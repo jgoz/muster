a65792bb3fa204d6f9b3d67f0a2ec06cb10480fa
John Gozde <johnny@jgoz.net>
2011-10-16 17:50:07 -0600

    Update name in assembly info.

--- src/GlobalAssemblyInfo.cs
+++ src/GlobalAssemblyInfo.cs
@@ -3,9 +3,9 @@ using System.Reflection;
 using System.Resources;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyCompany("Johnny Gozde")]
+[assembly: AssemblyCompany("John Gozde")]
 [assembly: AssemblyProduct("Muster")]
-[assembly: AssemblyCopyright("Copyright © Johnny Gozde 2011")]
+[assembly: AssemblyCopyright("Copyright © John Gozde 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: AssemblyConfiguration("")]
